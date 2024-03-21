6615419cec42de14743427d741cc8d0cdf4c58f5
ayushukl <shuklaayush00@gmail.com>
2024-03-21 22:16:02 +0530

    upated

--- node3/provider2.py
+++ node3/provider2.py
@@ -4,19 +4,19 @@ import socket
  
 # Function to update file on server
 def update_file(file_name, content, server, port):
-   try:
-       proxy = xmlrpc.client.ServerProxy(f"http://{server}:{port}/")
-       # Update file on server
-       proxy.update_file(file_name, content, server_path)
-       print(f"File created successfully")
-   except Exception as e:
-       print(f"Error updating file on server {server}: {e}")
+    try:
+        proxy = xmlrpc.client.ServerProxy(f"http://{server}:{port}/")
+        # Update file on server
+        proxy.update_file(file_name, content, server_path)
+        print(f"File created successfully")
+    except Exception as e:
+        print(f"Error updating file on server {server}: {e}")
 
 if __name__ == "__main__":
-   server = '172.31.5.56'
-   port = '8003'
-   current_working_directory = os.getcwd()
-   server_path = os.path.join(current_working_directory, 'server_files3')
-   file_name = input("Enter file name: ")
-   file_content = input("Enter file content: ")
-   update_file(file_name, file_content, server, port)
+    server = '172.31.5.56'
+    port = '8003'
+    current_working_directory = os.getcwd()
+    server_path = os.path.join(current_working_directory, 'server_files3')
+    file_name = input("Enter file name: ")
+    file_content = input("Enter file content: ")
+    update_file(file_name, file_content, server, port)
--- node3/server3.py
+++ node3/server3.py
@@ -7,34 +7,34 @@ NEAREST_SERVER = ['172.31.13.233:8002']
  
 # Function to check if file exists in server's file system
 def file_exists(file_path):
-   return os.path.isfile(file_path)
+    return os.path.isfile(file_path)
  
 # Function to retrieve file content
 def get_file_content(file_path):
-   with open(file_path, 'r') as file:
-       content = file.read()
-   return content
+    with open(file_path, 'r') as file:
+        content = file.read()
+        return content
  
 # RPC Server class
 class FileServerRPC:
-   def __init__(self, server_id, file_directory):
-       self.server_id = server_id
-       self.file_directory = file_directory
+    def __init__(self, server_id, file_directory):
+        self.server_id = server_id
+        self.file_directory = file_directory
   
-   def get_file(self, file_name, client_local_path):
-       file_path = os.path.join(self.file_directory, file_name)
-       if file_exists(file_path):
-           print(f"\nFile {file_name} is found in server {self.server_id}:{port}")
-           file_content = get_file_content(file_path) + "\nServer - " + self.server_id + ":" + str(port)
-       else:
-           print(f"\nFile {file_name} not found in server {self.server_id}:{port}")
-           return "\nFile not found"
+    def get_file(self, file_name, client_local_path):
+        file_path = os.path.join(self.file_directory, file_name)
+        if file_exists(file_path):
+            print(f"\nFile {file_name} is found in server {self.server_id}:{port}")
+            file_content = get_file_content(file_path) + "\nServer - " + self.server_id + ":" + str(port)
+        else:
+            print(f"\nFile {file_name} not found in server {self.server_id}:{port}")
+            return "\nFile not found"
  
-   def update_file(self, file_name, content, pathname):
-       file_path = os.path.join(pathname, file_name)
-       with open(file_path, 'w') as file:
-           file.write(content)
-       return "File updated successfully"
+    def update_file(self, file_name, content, pathname):
+        file_path = os.path.join(pathname, file_name)
+        with open(file_path, 'w') as file:
+            file.write(content)
+            return "File updated successfully"
  
  
 # Start RPC Server
@@ -49,8 +49,8 @@ def start_rpc_server(server_id, port, file_directory):
         sys.exit(0)
  
 if __name__ == "__main__":
-   address = '172.31.5.56'
-   port = 8003
-   current_working_directory = os.getcwd()
-   server_local_path = os.path.join(current_working_directory, 'server_files3')
-   start_rpc_server(address, port, server_local_path)
+    address = '172.31.5.56'
+    port = 8003
+    current_working_directory = os.getcwd()
+    server_local_path = os.path.join(current_working_directory, 'server_files3')
+    start_rpc_server(address, port, server_local_path)
