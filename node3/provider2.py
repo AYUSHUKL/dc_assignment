4f1d602b030a85e792445e8abda574e00ebb3e6d
Ayush Shukla <80712298+AYUSHUKL@users.noreply.github.com>
2024-03-21 09:16:47 +0530

    Update provider2.py

--- node3/provider2.py
+++ node3/provider2.py
@@ -13,7 +13,7 @@ def update_file(file_name, content, server, port):
        print(f"Error updating file on server {server}: {e}")
 
 if __name__ == "__main__":
-   server = 'localhost'
+   server = '172.31.11.63'
    port = '8003'
    current_working_directory = os.getcwd()
    server_path = os.path.join(current_working_directory, 'server_files3')
